9cc863e27676cedcdb0bfec61594402d434e9c72
shavo <weslleypereira.ti@gmail.com>
2020-03-11 00:01:59 -0300

    Add exercícios

--- exercicios-js/array/reduce3.js
+++ exercicios-js/array/reduce3.js
@@ -1,6 +1,7 @@
-Array.prototype.reduce2 = function(callback) {
-    let acumulador = this[0]
-    for(let i = 1; this.length; i++) {
+Array.prototype.reduce2 = function(callback, valorInicial) {
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for(let i = indiceInicial; i < this.length; i++) {
         acumulador = callback(acumulador, this[i], i, this)
     }
     return acumulador
@@ -8,4 +9,4 @@ Array.prototype.reduce2 = function(callback) {
 
 const soma = (total, valor) => total + valor
 const nums = [1, 2, 3, 4, 5, 6]
-console.log()
+console.log(nums.reduce2(soma, 21))
