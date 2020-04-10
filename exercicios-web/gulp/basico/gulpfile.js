77d92626a3b120a2ed99e79f501d7690d17b0c89
weslleyshavo <weslleypereira.ti@gmail.com>
2020-04-10 20:08:22 -0300

    Add Ajax Dom and Gulp

---
+++ exercicios-web/gulp/basico/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp')
+const { series, parallel } = require('gulp')
+
+const antes1 = cb => {
+    console.log('Tarefa antes 1!')    
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes 2!')    
+    return cb()
+}
+
+function copiar(cb) {
+    gulp.src('pastaA/**/*.txt')
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // .pipe(transformacaoC())
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')    
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+)
